40dfd0aa18b85de2aa40cbcf8a6c4df72d98aefa
lucasandrad <lucasandrad@yandex.com>
2018-08-25 13:14:43 -0300

    Create pages and start to create form to show validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
+  
+  # Pages
+  root 'pages#home'
+  get 'about' => 'pages#about', as: 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
